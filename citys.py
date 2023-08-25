d96d7162a64ac035a6e4cd4d9e74894dc477af94
JacobStadler <stadler.jacob.allan@gmail.com>
2023-08-25 10:46:42 -0400

    added more stuff to make the program actually do something

--- citys.py
+++ citys.py
@@ -31,7 +31,6 @@ Authority1 = ['Single ruler','Single ruler with inherited council','Single ruler
 class City():
     def __init__(self,name):
         self.name = name
-        self.leadership_type = 0
         self.population = 0
         self.residents = []
         self.neighbors = []
@@ -49,6 +48,16 @@ class City():
         self.resource= r.randint(0,7)
     def add_citizen(self,civ):
         self.residents.append(civ)
+        self.population += 1
+    def rem_citizen(self,civ):
+        i = 0
+        while i < len(self.residents):
+            if self.residents[i].id == civ.id:
+                #print(f'removed {self.residents[i].id}  | intended : {civ.id}')
+                self.residents.pop(i)
+                break
+            i += 1
+        self.population -= 1
     def add_neighbor(self,insert):
         self.neighbors.append(insert)
 
@@ -79,6 +88,35 @@ class Citizen():
         self.resource_pref = r.randint(0,7)
     def add_occupance(self,city):
         self.occupy = city
+    def move(self):
+        self.like_current = self.like(self.occupy)
+    def like(self,city):
+        total = 11
+        metch = 0
+        if self.trade_pref == city.trade:
+            metch += 1
+        if self.genderroles_pref == city.genderroles:
+            metch += 1
+        if self.event_pref == city.event:
+            metch += 1
+        if self.religion_pref == city.religion:
+            metch += 1
+        if self.authority_pref == city.authority:
+            metch += 1
+        if self.caste_pref == city.caste:
+            metch += 1
+        if self.language_pref == city.language:
+            metch += 1
+        if self.food_pref == city.food:
+            metch += 1
+        if self.art_pref == city.art:
+            metch += 1
+        if self.music_pref == city.music:
+            metch += 1
+        if self.resource_pref == city.resource:
+            metch += 1
+        #print(round(metch/total,2))
+        return round(metch/total,2)*100
 
 tom = City('tom')
 randy = City('randy')
@@ -108,19 +146,83 @@ man.add_neighbor(tom)
 man.add_neighbor(randy)
 man.add_neighbor(joel)
 man.add_neighbor(brad)
+total_amount_of_people = 500
 ppl = []
-for i in range(100):
+for i in range(total_amount_of_people):
     ppl.append(Citizen(i))
 num_of_ppl = 0
-while num_of_ppl < 100:
+while num_of_ppl < total_amount_of_people:
     city = r.randint(0,4)
     cities[city].add_citizen(ppl[num_of_ppl])
+    ppl[num_of_ppl].occupy = cities[city]
     num_of_ppl += 1
 
-man_r = len(man.residents)
+"""man_r = len(man.residents)
 counter = 0
 while counter < man_r:
     print(man.residents[counter].id)
-    counter += 1
-#for i in man.residents:
-#    print(man.residents[1])
+    counter += 1"""
+#occ = ppl[1].occupy
+#print(occ.name)
+#print(ppl[1].like(occ))
+
+cycles = 3
+while cycles > 0:
+    moves = 0
+    civs = 100
+    while civs > 0:
+        pers = ppl[civs-1]
+        civ_like_store = []
+        civ_like_score = []
+        civ_city = pers.occupy
+        #print(f'{pers.id} lives in {civ_city.name}')
+        civ_like_store.append(pers.occupy)
+        civ_like_score.append(pers.like(pers.occupy))
+        highest_like = 0
+        civ_city_n = len(civ_city.neighbors)
+        i = 0
+        while i < civ_city_n:
+            civ_like_store.append(civ_city.neighbors[i])
+            civ_like_score.append(pers.like(civ_city.neighbors[i]))
+            if civ_like_score[i] > civ_like_score[highest_like]:
+                highest_like = i
+            i += 1
+        if highest_like != 0:
+            moves += 1
+            print(f'{pers.id} moved from {civ_city.name} to {civ_city.neighbors[highest_like-1].name}')
+            civ_city.rem_citizen(pers)
+            civ_city.neighbors[highest_like-1].add_citizen(pers)
+            pers.add_occupance(civ_city.neighbors[highest_like-1])
+            #print(f'{pers.occupy.name}')
+        civs -= 1
+    print(f'\nmoves : {moves}\n')
+    cycles -= 1
+
+def city_info(c):
+    i = 0
+    total_aproval = 0
+    while i < len(c.residents):
+        total_aproval += c.residents[i].like(c)
+        i += 1
+    print(f'''
+    City :          {c.name}
+    Avg Approval :   {round(total_aproval/c.population,2)}
+    Pop:            {c.population}
+    Auth:           {Authority0[c.authority[0]]} {Authority1[c.authority[1]]}
+    Trade:          {Trade[c.trade]}
+    Gender Roles:   {GenderRolls[c.genderroles]}
+    Events :        {Events[c.event]}
+    Religion :      {Religion[c.religion]}
+    Caste :         {Caste[c.caste]}
+    Language :      {Language[c.language]}
+    Food :          {Food[c.food]}
+    Art:            {Art[c.art]}
+    Music :         {Music[c.music]}
+    Resource :      {Resource[c.resource]}
+        ''')
+
+city_info(tom)
+city_info(randy)
+city_info(joel)
+city_info(brad)
+city_info(man)
