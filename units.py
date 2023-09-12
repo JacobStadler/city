ccbc4d4a7941f239b7f6ae314265a84b7859ed4e
JacobStadler <stadler.jacob.allan@gmail.com>
2023-09-12 19:48:13 -0400

    more unpdate to classes

--- units.py
+++ units.py
@@ -19,11 +19,27 @@ class Unit():
         self.gclass = None
         self.inventory = ['clothes']
         self.wares = []
+        self.mainhand = []
+        self.belifes = []
 
-    def unarmed_attack(self,target):
+    def upkeep(self):
+        if not self.concious:
+            wake_up = r(1,100)
+            if wake_up == 100:
+                self.health = 10
+
+    def targeted(self,attacker):
+        print(f'{self.name} was targeted by {attacker.name}')
+
+    def attack(self,target):
+        target.targeted(self)
         roll = r(1,100)
         if roll >= target.ac:
-            target.health -= self.strength
+            if len(self.mainhand) == 0:
+                damage = self.strength
+            else:
+                damage = self.mainhand[0].roll_damage
+            target.health -= damage
             if target.health <= 0:
                 target.health = 0
                 target.concious = False
@@ -38,4 +54,32 @@ class Unit():
         else:
             print(f'{target.name} has nothing to trade')
 
+class Rager(Unit):
+    def upkeep(self):
+        print('temp')
+
+    def start_rage(unit):
+        unit.rage_for = 1
+        unit.is_raging = True
     
+    def end_rage(unit):
+        unit.rage_for = 0
+        unit.is_raging = False
+
+class Assasin(Unit):
+    def attack(self,target):
+        roll = r(1,100)
+        if roll >= target.ac:
+            if len(self.mainhand) == 0:
+                damage = self.strength
+            else:
+                damage = ((r(1,4)+r(1,4)+r(1,4)+r(1,4))*10)+self.mainhand[0].roll_damage
+            target.health -= damage
+            if target.health <= 0:
+                target.health = 0
+                target.concious = False
+
+class Bard(Unit):
+    def convince(self,belief,target):
+        # you can change/give a target (non player) belief
+        target.belief.append(belief)
