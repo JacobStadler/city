56e0ab72c9abead4342d5493ca5b9f953d7ad643
JacobStadler <stadler.jacob.allan@gmail.com>
2024-08-18 17:37:10 -0400

    updated workflow

--- newcity/squaredWorld.py
+++ newcity/squaredWorld.py
@@ -2,26 +2,32 @@ from random import randint as ri
 import numpy as np
 from PIL import Image, ImageDraw
 np.set_printoptions(threshold=10000)
-size = 1024
+size = 4000
+height = 5000
+width = 10000
 
-m = np.zeros(shape=(size,size))
+m = np.zeros(shape=(height,width))
 
 #disp[0][size-1] = 5
 
 def build():
-    start = ri(1,size-1)
-    #   y        x
-    m[start-1][start-1] += 1
-    m[start-1][start  ] += 1
-    m[start  ][start-1] += 1
-    m[start  ][start  ] += 1
+    start_width = ri(1,width-1)
+    start_height = ri(1,height-1)
+    #      y               x
+    m[start_height-1][start_width-1] += 1
+    m[start_height-1][start_width  ] += 1
+    m[start_height  ][start_width-1] += 1
+    m[start_height  ][start_width  ] += 1
     # should be size squared (size*size)
     # ** = exponent
-    total_blocks = size**2+size**2+size**2
+    total_blocks = (height*width)**2+(height*width)**2+(height*width)**2
+    total_blocks = (height*width)**2
     # pos is [4,4]
-    pos = [start,start]
+    pos = [start_height,start_width]
     #print(pos)
     choice_log = []
+    progress = 0
+    complete = (height*width)**2+(height*width)**2+(height*width)**2
     while total_blocks:
         direction = ri(1,4)
         #print(pos)
@@ -37,7 +43,7 @@ def build():
                 m[pos[0]-2][pos[1]-1] += 1 # [3,2]
                 m[pos[0]-1][pos[1]-1] += 1 # [3,3]
                 pos[0] -= 1
-                choice_log += ['U '+str(pos)]
+                #choice_log += ['U '+str(pos)]
             case 2:
                 if pos[1]+1 >= size-1:
                     pos[1] = 0
@@ -49,7 +55,7 @@ def build():
                 m[pos[0]-1][pos[1]+1] += 1 # [5,3]
                 m[pos[0]  ][pos[1]+1] += 1 # [5,4]
                 pos[1] += 1
-                choice_log += ['R '+str(pos)]
+                #choice_log += ['R '+str(pos)]
             case 3:
                 if pos[0]+1 >= size-1:
                     pos[0] = 0
@@ -61,7 +67,7 @@ def build():
                 m[pos[0]+1][pos[1]-1] += 1 # [3,5]
                 m[pos[0]  ][pos[1]+1] += 1 # [4,5]
                 pos[0] += 1
-                choice_log += ['D '+str(pos)]
+                #choice_log += ['D '+str(pos)]
             case 4:
                 if pos[1]-1 < 0:
                     pos[1] = size-1
@@ -73,9 +79,17 @@ def build():
                 m[pos[0]-1][pos[1]-2] += 1 # [2,3]
                 m[pos[0]  ][pos[1]-2] += 1 # [2,4]
                 pos[1] -= 1
-                choice_log += ['L '+str(pos)]
+                #choice_log += ['L '+str(pos)]
         total_blocks -= 1
+
+        if (progress/complete) % 500 == 0:
+            print(f'{(progress/complete)/10000}%')
+        #print(f'{round((progress/complete),10)}\t\t{progress}/{complete}')
+        progress += 1
     #print(choice_log)
+    print('done')
+    return m
+
 def draw():
     pixel = 1
     img = Image.new("RGBA", (size * pixel, size * pixel), (100, 100, 100, 100))
@@ -85,10 +99,13 @@ def draw():
             capture = int(m[j][i])
             clr = (capture,capture,capture)
             draw.rectangle((i,j,i+1,j+1),fill=clr,width=pixel)
-    img.save('.\world.png')
-build()
-draw()
+    img.save('world.png')
+
+if __name__ == '__main__':
+    build()
+
+#draw()
 #m = np.array2string(m,max_line_width=1000,separator='')
 #print(m)
-print()
-print('done')
+#print()
+#print('done')
---
+++ newcity/tempature_pressure_humidity.py
@@ -0,0 +1,4 @@
+import squaredWorld
+
+map = squaredWorld.build()
+
